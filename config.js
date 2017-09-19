89fe6239bb581dfcde604ad072e6d11367a76e45
Matt <[mailprogramisty@gmail.com]>
2017-09-19 14:12:15 +0200

    Task 17.7 Auth Google added. Have some problem

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '79259527442-b8552a7har04huteh408oo9umpgg3f1k.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'FGgaL6CiQIiBZZzqVQ2STc_N',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
